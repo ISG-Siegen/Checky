753fe680d92456b2791bbf9099ef4e16a07018e1
Moritz Baumgart <baumgartmoritz@web.de>
2024-07-10 13:32:11 +0200

    fix: load/show swagger only in development mode; Change swagger title

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['dev' => true, 'test' => true],
 ];
