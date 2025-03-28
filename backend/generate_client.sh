ea7d5b8ad54d72bec896caec6538dbc604b2413e
Moritz Baumgart <baumgartmoritz@web.de>
2025-03-28 14:40:48 +0100

    build: adapt client generation script to new directory structure

--- backend/generate_client.sh
+++ backend/generate_client.sh
@@ -1,1 +1,1 @@
-npx openapi-generator-cli generate -i http://127.0.0.1:8000/api/doc.json -g typescript-angular -o ../checklist/src/app/api --additional-properties fileNaming=kebab-case
+npx openapi-generator-cli generate -i http://127.0.0.1:8000/api/doc.json -g typescript-angular -o ../frontend/src/app/api --additional-properties fileNaming=kebab-case
